a7db915cd9951d5c8e770fa83745fb8078e4e62f
leofer28 <62780867+leofer28@users.noreply.github.com>
2020-06-14 19:13:26 -0300

    Add files via upload

---
+++ funcoes_mongo_php/controllers/produtos/add.php
@@ -0,0 +1,9 @@
+<?php
+
+    require_once __DIR__."/../../core/mongo/findall.php";
+
+    $collection='ecommerce.marcas';
+    
+    $marcas=findAll($collection);
+
+    require_once __DIR__."/../../views/produtos/add.php";
